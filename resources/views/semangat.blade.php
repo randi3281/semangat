114bd5ff152e73054cc33d1e95ac9dac18847402
anfi3281 <anfi3281@gmail.com>
2023-03-20 19:58:54 +0700

    new

---
+++ resources/views/semangat.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Kata-kata Semangat</title>
+</head>
+
+<body>
+    <h1>Halo Kamu</h1>
+    <p>Iya Kamu, yang lagi berjuang di tengah ekspetasi mereka. Kamu gausah peduli atas semua hal-hal yang membuatmu
+        down. Mereka hanya tahu mengenai luarmu, bukan dalam dirimu. Kamu pasti akan merasa sakit kalau terlalu
+        memikirkannya.</p>
+</body>
+
+</html>
