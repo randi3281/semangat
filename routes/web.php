410a97b668778d2b0853c7e3b4f9b3416af8e0fd
anfi3281 <anfi3281@gmail.com>
2023-03-23 10:31:32 +0700

    portofolio

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,7 @@ use Illuminate\Support\Facades\Auth;
 */
 // Belajar
 Route::get('/', function () {
-    // return view('welcome');
-    return "hys";
+    return view('portofolio.portofolio');
 });
 Route::get('/semangat', 'SemangatController@index');
 Route::get('/belajar4', 'SemangatController@belajar4');
