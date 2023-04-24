df40dc5ca2a5439212d8426fc5036a26ccda96bd
anfi3281 <anfi3281@gmail.com>
2023-04-24 12:48:33 +0700

    ganti bahasa

--- resources/views/anficititate/del_repo.blade.php
+++ resources/views/anficititate/del_repo.blade.php
@@ -1,24 +1,24 @@
-<h1 class="text-center fs-3 mt-5 mb-5" style="font-family: 'Times New Roman', Times, serif">DELETE REPOSITORY</h1>
+<h1 class="text-center fs-3 mt-5 mb-5" style="font-family: 'Times New Roman', Times, serif">HAPUS REPOSITORY</h1>
 <div class="row justify-content-center">
     <div class="col-sm-8 ">
         <form action="pin" method="POST">
             <div class="form-group">
-                <label for="repository">Repository</label>
+                <label for="repository">Repositori</label>
                 {{-- <input id="my-input" class="form-control" type="text" name=""> --}}
                 <select class="form-select" name="repository" id="">
                     <option value="hy" selected>hy</option>
                 </select>
             </div>
-            <p style="font-size: 7pt">Or want to <a href="slc_repo">Select Repository</a> or <a href="upd_repo">Update
-                    Repository</a></p>
+            <p style="font-size: 7pt">Atau ingin untuk <a href="slc_repo">Pilih Repository</a> atau <a class="text-danger" href="upd_repo">Perbarui
+                    Repositori</a></p>
             <div class="form-group mt-3">
                 <label for="pin">PIN</label>
-                <input id="pin" class="form-control" type="password" name="pin">
+                <input id="pin" class="form-control" type="password" maxlength="6" name="pin">
             </div>
             <div class="text-center mt-5">
                 <input type="submit" style="height: 38px; font-size: 12px; font-weight: bold"
-                    class="btn btn-danger w-50" name="delete" value="DELETE">
-                <p class="mt-2" style="font-size: 7pt">Want to change account? <a href="login">Login again Here</a>
+                    class="btn btn-danger w-50" name="delete" value="HAPUS">
+                <p class="mt-2" style="font-size: 7pt">Ingin Mengganti Akun? <a href="login">Login Kembali Disini</a>
                 </p>
             </div>
         </form>
--- resources/views/anficititate/upd_repo.blade.php
+++ resources/views/anficititate/upd_repo.blade.php
@@ -1,35 +1,36 @@
-<h1 class="text-center fs-3 mt-5 mb-2" style="font-family: 'Times New Roman', Times, serif">UPDATE REPOSITORY</h1>
+<h1 class="text-center fs-3 mt-5 mb-2" style="font-family: 'Times New Roman', Times, serif">PERBARUI REPOSITORI</h1>
 <div class="row justify-content-center">
     <div class="col-sm-8 ">
         <form action="pin" method="POST">
             <div class="form-group">
                 <div class="row text-center justify-content-center" style="font-size: 9pt; margin-bottom: -20px">
                     <p>
-                        <a class="text-white">-</a><a class="text-danger" style="text-decoration: none">s</a><a
+                        <a class="text-white">-</a><a class="text-danger" style="text-decoration: none"></a><a
                             class="text-white">-</a>
                     </p>
                 </div>
-                <label for="repository">Repository</label>
+                <label for="repository">Repositori</label>
 
                 <select class="form-select" name="repository" id="">
                     <option value="hy" selected>hy</option>
                 </select>
             </div>
-            <p style="font-size: 7pt; margin-bottom:-5px;">Or want to <a href="/anficititate/del_repo" class="text-danger">Delete
-                    Repository</a> or <a href="/anficititate/slc_repo">Select Repository</a></p>
+            <p style="font-size: 7pt; margin-bottom:-5px;">Atau ingin untuk <a href="/anficititate/slc_repo">Pilih
+                    Repositori</a> atau <a href="/anficititate/del_repo" class="text-danger">Hapus
+                    Repositori</a></p>
             <div class="form-group mt-2">
                 <input id="namabaru" class="form-control" type="text" name="namabaru"
-                    placeholder="Input new name here ...">
+                    placeholder="Masukkan nama repositori yang baru">
             </div>
             <div class="form-group mt-3">
                 <label for="pin">PIN</label>
-                <input id="pin" class="form-control" type="password" name="pin">
+                <input id="pin" class="form-control" maxlength="6" type="password" name="pin">
             </div>
             <div class="text-center mt-4">
                 <input type="submit" name="enter" style="height: 38px; font-size: 12px; font-weight: bold"
                     class="btn btn-primary w-50" value="ENTER">
-                <p class="mt-2" style="font-size: 7pt">Want to change account? <a href="/anficititate">Login again
-                        Here</a>
+                <p class="mt-2" style="font-size: 7pt">Ingin Mengganti Akun? <a href="login">Login Kembali
+                        Disini</a>
                 </p>
             </div>
         </form>
