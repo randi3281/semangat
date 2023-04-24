139f9be51df80849aaadf2e878ec7fc8a32647c8
anfi3281 <anfi3281@gmail.com>
2023-04-24 11:40:19 +0700

    selectrepo

--- resources/views/anficititate/slc_repo.blade.php
+++ resources/views/anficititate/slc_repo.blade.php
@@ -1,27 +1,27 @@
-<h1 class="text-center fs-3 mt-5 mb-5" style="font-family: 'Times New Roman', Times, serif">SELECT REPOSITORY</h1>
+<h1 class="text-center fs-3 mt-5 mb-5" style="font-family: 'Times New Roman', Times, serif">PILIH REPOSITORI</h1>
 <div class="row justify-content-center">
     <div class="col-sm-8 ">
         <form action="home" method="POST">
             {{ csrf_field() }}
             <div class="form-group">
-                <label for="repository">Repository</label>
+                <label for="repository">Repositori</label>
                 {{-- <input id="my-input" class="form-control" type="text" name=""> --}}
                 <select class="form-select" name="repository" id="" onchange="javascript:handleselect(this)">
                     <option value="hy" selected>hy</option>
-                    <option value="hy">Create New Repository</option>
+                    <option value="hy">Buat Repositori baru</option>
                 </select>
             </div>
-            <p style="font-size: 7pt">Or want to <a href="/anficititate/del_repo" class="text-danger">Delete Repository</a> or <a
-                    href="/anficititate/upd_repo">Update
-                    Repository</a></p>
+            <p style="font-size: 7pt">Atau ingin untuk <a href="/anficititate/del_repo" class="text-danger">Hapus Repositori</a> atau <a
+                    href="/anficititate/upd_repo">Perbarui
+                    Repositori</a></p>
             <div class="form-group mt-3">
                 <label for="pin">PIN</label>
-                <input id="pin" class="form-control" type="password" name="pin">
+                <input id="pin" class="form-control" type="password" maxlength="6" name="pin">
             </div>
             <div class="text-center mt-5">
                 <input type="submit" style="height: 38px; font-size: 12px; font-weight: bold"
-                    class="btn btn-primary w-50" name="enter" value="ENTER">
-                <p class="mt-2" style="font-size: 7pt">Want to change account? <a href="/anficititate/back">Login again Here</a>
+                    class="btn btn-primary w-50" name="enter"  value="MASUK">
+                <p class="mt-2" style="font-size: 7pt">Ingin Mengganti Akun? <a href="/anficititate/back">Login Kembali Disini</a>
                 </p>
             </div>
         </form>
