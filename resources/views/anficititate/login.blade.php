35d315ac8ea63bf7376141a7ef01cad65d4d9c57
anfi3281 <anfi3281@gmail.com>
2023-04-24 18:52:40 +0700

    lupakatasandi

--- resources/views/anficititate/login.blade.php
+++ resources/views/anficititate/login.blade.php
@@ -1,5 +1,5 @@
-<h1 class="text-center fs-3 mt-4 mb-2" style="font-family: 'Times New Roman', Times, serif">LOGIN</h1>
-<div class="row text-center justify-content-center" style="font-size: 9pt; margin-bottom: -17px">
+<h1 class="text-center fs-3 mt-3 mb-2" style="font-family: 'Times New Roman', Times, serif">LOGIN</h1>
+<div class="row text-center justify-content-center" style="font-size: 9pt; margin-top:-10px; margin-bottom: 10px">
     <p>
         <a class="text-white">-</a>
         <a class="text-danger" style="text-decoration: none">
@@ -13,7 +13,7 @@
     </p>
 </div>
 
-<div class="row justify-content-center">
+<div class="row justify-content-center" style="margin-top: -32px">
     <div class="col-sm-8 ">
         <form action="/anficititate/slc_repo" method="POST">
             {{ csrf_field() }}
@@ -25,6 +25,7 @@
                 <label for="password">Kata Sandi</label>
                 <input id="password" class="form-control" type="password" name="password" required>
             </div>
+            <p style="font-size: 7pt">Lupa Kata Sandi? <a href="/anficititate/del_repo">Klik Disini</a></p>
             <div class="form-group mt-3">
                 <label style="margin-right: 12px">Captcha</label>
                 <img src="/captcha/anficititate/captcha.php" alt="gambar">
@@ -33,7 +34,7 @@
                 <input id="captcha" class="form-control" type="text" name="captcha" placeholder="Isi Captcha ..."
                     maxlength="6" value="" required>
             </div>
-            <div class="text-center mt-4">
+            <div class="text-center mt-3">
                 <input type="submit" style="height: 38px; font-size: 12px; font-weight: bold"
                     class="btn btn-primary w-50" value="MASUK">
                 <p class="mt-2" style="font-size: 7pt">Belum Mempunyai Akun? <a href="/anficititate/daftar">Daftar
