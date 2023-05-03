74173aec6b1a88dc3402971876d2e6db2f25ff36
anfi3281 <anfi3281@gmail.com>
2023-05-04 04:25:11 +0700

    div

--- resources/views/anficititate/kelola/editwebsite.blade.php
+++ resources/views/anficititate/kelola/editwebsite.blade.php
@@ -1,31 +1,28 @@
+{{-- Form Website --}}
+</div>
+<input type="hidden" name="idedita" value="{{ $edita->id }}">
+<div class="form-group  mt-3">
+    <label for="judul_web">Judul Web</label> <br>
+    <textarea name="judul_web" class="form-control" style="font-size : 11px;" cols="30" rows="3"
+        placeholder="Tuliskan Judul">{{ $edita->judul_web }}</textarea>
+</div>
 
-                                        {{-- Form Website --}}
-                                    </div>
-                                    <input type="hidden" name="idedita" value="{{ $edita->id }}">
-                                    <div class="form-group  mt-3">
-                                        <label for="judul_web">Judul Web</label> <br>
-                                        <textarea name="judul_web" class="form-control" style="font-size : 11px;" cols="30" rows="3"
-                                            placeholder="Tuliskan Judul">{{ $edita->judul_web }}</textarea>
-                                    </div>
+<div class="mt-3 form-group">
+    <label for="deskripsi_web">Deskripsi Web</label>
+    <textarea type="text" class="form-control" style="font-size : 11px;" cols="30" rows="3"
+        name="deskripsi_web" placeholder="Deskripsi">{{ $edita->deskripsi_web }}</textarea>
+</div>
 
-                                    <div class="mt-3 form-group">
-                                        <label for="deskripsi_web">Deskripsi Web</label>
-                                        <textarea type="text" class="form-control" style="font-size : 11px;" cols="30" rows="3"
-                                            name="deskripsi_web" placeholder="Deskripsi">{{ $edita->deskripsi_web }}</textarea>
-                                    </div>
+<div class="mt-2 form-group">
+    <input type="text" class="form-control" name="tahun_web" placeholder="Tahun" value="{{ $edita->tahun_web }}">
+</div>
 
-                                    <div class="mt-2 form-group">
-                                        <input type="text" class="form-control" name="tahun_web" placeholder="Tahun"
-                                            value="{{ $edita->tahun_web }}">
-                                    </div>
+<div class="mt-2 form-group">
+    <input type="text" class="form-control" name="link_web" placeholder="Link" value="{{ $edita->link_web }}">
+</div>
 
-                                    <div class="mt-2 form-group">
-                                        <input type="text" class="form-control" name="link_web" placeholder="Link"
-                                            value="{{ $edita->link_web }}">
-                                    </div>
-
-                                    <div class="mt-2 form-group">
-                                        <input type="text" class="form-control" name="tanggal_diakses_web"
-                                            placeholder="Tanggal Diakses" value="{{ $edita->tanggal_diakses_web }}">
-                                    </div>
-                                    {{-- End Form Website --}}
+<div class="mt-2 form-group">
+    <input type="text" class="form-control" name="tanggal_diakses_web" placeholder="Tanggal Diakses"
+        value="{{ $edita->tanggal_diakses_web }}">
+</div>
+{{-- End Form Website --}}
--- resources/views/anficititate/kelola/jurnal.blade.php
+++ resources/views/anficititate/kelola/jurnal.blade.php
@@ -1,7 +1,6 @@
 {{-- Form Jurnal --}}
 <label for="jumlah">Jumlah Penulis : </label>
-<select class="form-select-sm" name="jumlah_penulis" style=" width: 50px;"
-    onchange="javascript:handleselect(this)">
+<select class="form-select-sm" name="jumlah_penulis" style=" width: 50px;" onchange="javascript:handleselect(this)">
     {{-- Percabangan --}}
     @if (isset($jumlahpenulis))
         @if ($jumlahpenulis == 4)
@@ -35,49 +34,44 @@
 </div>
 
 <div class="mt-1 form-group">
-@if (isset($jumlahpenulis))
-    @if ($jumlahpenulis == 4)
-        <input type="text" class=" form-control" placeholder="Penulis 1"
-            name="penulis_1">
+    @if (isset($jumlahpenulis))
+        @if ($jumlahpenulis == 4)
+            <input type="text" class=" form-control" placeholder="Penulis 1" name="penulis_1">
+        @else
+            @for ($i = 1; $i <= $jumlahpenulis; $i++)
+                <input type="text" class="form-control" placeholder="Penulis {{ $i }}"
+                    name="penulis_{{ $i }}">
+            @endfor
+        @endif
     @else
-        @for ($i = 1; $i <= $jumlahpenulis; $i++)
-            <input type="text" class="form-control"
-                placeholder="Penulis {{ $i }}"
-                name="penulis_{{ $i }}">
-        @endfor
+        <input type="text" class="form-control" placeholder="Penulis 1" name="penulis_1">
     @endif
-@else
-    <input type="text" class="form-control" placeholder="Penulis 1" name="penulis_1">
-@endif
 </div>
 
 <div class="form-group  mt-2">
-<label for="judul">Judul</label> <br>
-<textarea name="judul" class="form-control"style="font-size : 11px;" id="" cols="30" rows="3"
-    placeholder="Tuliskan Judul"></textarea>
+    <label for="judul">Judul</label> <br>
+    <textarea name="judul" class="form-control"style="font-size : 11px;" id="" cols="30" rows="3"
+        placeholder="Tuliskan Judul"></textarea>
 </div>
 
 <div class="mt-1 form-group">
-<textarea name="sumber" class="form-control"style="font-size : 11px;" id="" cols="30" rows="2"
-    placeholder="Sumber"></textarea>
+    <textarea name="sumber" class="form-control"style="font-size : 11px;" id="" cols="30" rows="2"
+        placeholder="Sumber"></textarea>
 </div>
 
 <div class="mt-2 form-group">
-<input type="text" class="text-center form-control-sm" name="volume"
-    style="width:70px;" placeholder="Volume">
-<input type="text" class="text-center form-control-sm" name="nomor"
-    style="width:70px;" placeholder="Nomor">
-<input type="text" class="text-center form-control-sm" name="tahun"
-    style="width:70px;" placeholder="Tahun">
-<label for="halaman">Hal: </label>
-<input type="text" class="text-center form-control-sm float-left"
-    style="width:60px;" name="halaman_awal" placeholder="Awal">
--
-<input type="text" class="text-center form-control-sm float-left"
-    style="width:60px;" name="halaman_akhir" placeholder="Akhir">
+    <input type="text" class="text-center form-control-sm" name="volume" style="width:70px;" placeholder="Volume">
+    <input type="text" class="text-center form-control-sm" name="nomor" style="width:70px;" placeholder="Nomor">
+    <input type="text" class="text-center form-control-sm" name="tahun" style="width:70px;" placeholder="Tahun">
+    <label for="halaman">Hal: </label>
+    <input type="text" class="text-center form-control-sm float-left" style="width:60px;" name="halaman_awal"
+        placeholder="Awal">
+    -
+    <input type="text" class="text-center form-control-sm float-left" style="width:60px;" name="halaman_akhir"
+        placeholder="Akhir">
 </div>
 <div class="mt-2 form-group">
-<input type="text" class="form-control" name="kota" placeholder="Asal Kota">
+    <input type="text" class="form-control" name="kota" placeholder="Asal Kota">
 </div>
 
 {{-- End Form Jurnal --}}
