3cddcf77f6b957475ac1231c3e9dc29a2d91c1ea
anfi3281 <anfi3281@gmail.com>
2023-05-04 11:39:55 +0700

    NEW

--- resources/views/anficititate/new_repo.blade.php
+++ resources/views/anficititate/new_repo.blade.php
@@ -27,9 +27,9 @@
             </div>
             <div class="text-center mt-5">
                 <input type="submit" style="height: 38px; font-size: 12px; font-weight: bold"
-                    class="btn btn-primary w-50" name="new" value="BUAT BARU">
+                    class="btn btn-primary w-25" name="new" value="BUAT">
                 <input type="submit" style="height: 38px; font-size: 12px; font-weight: bold"
-                    class="btn btn-secondary w-25" name="enter" value="MASUK">
+                    class="btn btn-secondary w-50" name="enter" value="PILIH REPOSITORI">
                 <p class="mt-2" style="font-size: 7pt">Ingin Mengganti Akun? <a href="/anficititate/back">Login
                         Kembali Disini</a>
                 </p>
