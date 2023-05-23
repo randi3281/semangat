0e2f1a42156bcceddad01588ac4a7720de5f64ac
anfi3281 <anfi3281@gmail.com>
2023-05-23 10:11:42 +0700

    perapihan

--- resources/views/anficititate/tabel/index.blade.php
+++ resources/views/anficititate/tabel/index.blade.php
@@ -2,19 +2,19 @@
     <div class="card mb-3">
         <div class="card-body">
             <div class="row pb-0 justify-content-left">
-                <div class="pb-0 col-lg-7 mb-2 text-center">
+                <div class="pb-0 col-lg-8 mb-2 text-left">
                     <div>
                         <form action="/anficititate/kelola" method="POST">
                             <input type="hidden" name="urut" value="{{ $nomor }}">
                             {{ csrf_field() }}
-                            <input type="submit" name="keluar" value="Logout" class="btn btn-danger">
-                            <input type="submit" name="select" value="Pilih Repositori" class="btn btn-success">
-                            <input type="submit" name="rapi" value="Daftar Footnote" class="btn btn-primary">
-                            <input type="submit" name="dapus" value="Daftar Pustaka" class="btn btn-primary">
+                            <input type="submit" name="keluar" value="Logout" class="btn btn-danger  mb-1">
+                            <input type="submit" name="select" value="Pilih Repositori" class="btn btn-success mb-1">
+                            <input type="submit" name="rapi" value="Daftar Footnote" class="btn btn-primary mb-1">
+                            <input type="submit" name="dapus" value="Daftar Pustaka" class="btn btn-primary mb-1">
                         </form>
                     </div>
                 </div>
-                <div class="pb-0 text-center col-lg-5">
+                <div class="pb-0 text-center col-lg-4">
                     @if (isset($data))
                         {{ $data->links() }}
                     @endif
