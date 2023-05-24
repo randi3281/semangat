ed2accd7e556f279f51ca86a292ebbac5b5bec64
anfi3281 <anfi3281@gmail.com>
2023-05-24 11:12:04 +0700

    save

--- resources/views/anficititate/tabel/UINGUSDUR/dapus.blade.php
+++ resources/views/anficititate/tabel/UINGUSDUR/dapus.blade.php
@@ -1,13 +1,33 @@
 <h4 class="text-center mb-4">Daftar Pustaka</h4>
 <div style="font-size: 12pt; font-family: 'Times New Roman';">
     <?php
-    $_SESSION['cekjudul'] = array('halo');
+    $_SESSION['cekjudul'] = ['halo'];
     ?>
     @foreach ($datapus as $ft)
         @if ($ft->jenis == 1)
             @include('anficititate.tabel.UINGUSDUR.dapusnya.jurnal')
         @elseif ($ft->jenis == 2)
             @include('anficititate.tabel.UINGUSDUR.dapusnya.website')
+        @elseif ($ft->jenis == 3)
+            @include('anficititate.tabel.UINGUSDUR.dapusnya.buku')
+        @elseif ($ft->jenis == 4)
+            @include('anficititate.tabel.UINGUSDUR.dapusnya.terjemahan')
+        @elseif ($ft->jenis == 5)
+            @include('anficititate.tabel.UINGUSDUR.dapusnya.majalah')
+        @elseif ($ft->jenis == 6)
+            @include('anficititate.tabel.UINGUSDUR.dapusnya.suratKabar')
+        @elseif ($ft->jenis == 7)
+            @include('anficititate.tabel.UINGUSDUR.dapusnya.karanganTidakDiterbitkan')
+        @elseif ($ft->jenis == 8)
+            @include('anficititate.tabel.UINGUSDUR.dapusnya.wawancara')
+        @elseif ($ft->jenis == 9)
+            @include('anficititate.tabel.UINGUSDUR.dapusnya.pidato')
+        @elseif ($ft->jenis == 10)
+            @include('anficititate.tabel.UINGUSDUR.dapusnya.komentar')
+        @elseif ($ft->jenis == 11)
+            @include('anficititate.tabel.UINGUSDUR.dapusnya.ensiklopedia')
+        @elseif ($ft->jenis == 12)
+            @include('anficititate.tabel.UINGUSDUR.dapusnya.makalah')
         @endif
     @endforeach
 </div>
---
+++ resources/views/anficititate/tabel/UINGUSDUR/dapusnya/buku.blade.php
@@ -0,0 +1,69 @@
+@php
+    $kalimat = '';
+    $kalimat2 = '';
+    $izin = 0;
+    for ($i = 0; $i <= count($_SESSION['cekjudul']) - 1; $i++) {
+        if ($ft->judul == $_SESSION['cekjudul'][$i]) {
+            $izin = 0;
+            break;
+        } else {
+            $izin = 1;
+            // break;
+        }
+    }
+
+    if ($izin == 1) {
+        array_push($_SESSION['cekjudul'], $ft->judul);
+        $kal = $ft->penulis_1;
+        $kali = explode(' ', $kal);
+        $lika = end($kali);
+        $kalimat = $lika;
+        if ($lika == $ft->penulis_1) {
+        } else {
+            $kalimat .= ', ';
+        }
+        foreach ($kali as $kila) {
+            if ($kila == $lika) {
+            } else {
+                $kalimat .= ' ';
+                $kalimat .= $kila;
+            }
+        }
+        if ($ft->jumlah_penulis == 3) {
+            $kalimat .= ', ';
+            $kalimat .= $ft->penulis_2;
+            $kalimat .= ', dan ';
+            $kalimat .= $ft->penulis_3;
+            $kalimat .= '. (';
+        } elseif ($ft->jumlah_penulis == 2) {
+            $kalimat .= ', ';
+            $kalimat .= $ft->penulis_2;
+            $kalimat .= '. (';
+        } elseif ($ft->jumlah_penulis == 1) {
+            $kalimat .= '. (';
+        } else {
+            $kalimat .= ' dkk. (';
+        }
+        $kalimat .= $ft->tahun;
+        $kalimat .= '). ';
+        $kalimat .= $ft->judul;
+        $kalimat .= '. ';
+        $kalimat2 .= ' (';
+        $kalimat2 .= $ft->nomor;
+        $kalimat2 .= '), ';
+
+        if (isset($ft->halaman_akhir)) {
+            $kalimat2 .= $ft->halaman_awal;
+            $kalimat2 .= '-';
+            $kalimat2 .= $ft->halaman_akhir;
+        } else {
+            $kalimat2 .= $ft->halaman_awal;
+        }
+        $kalimat2 .= '.';
+    }
+@endphp
+<a style="text-align: justify">
+    @if ($izin == 1)
+        {{ $kalimat }}<i>{{ $ft->sumber }}</i>, <i>{{ $ft->volume }}</i>{{ $kalimat2 }}<br />
+    @endif
+</a>
