1baf8abaf75212631a8cda6a44257e7246c32133
anfi3281 <anfi3281@gmail.com>
2023-08-11 07:16:39 +0700

    tambah garis lurus

--- resources/views/pssp/index.blade.php
+++ resources/views/pssp/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>E-Perpus - SDN 1 Planjan</title>
+    <title>E-Perpus | SDN 1 Planjan</title>
     <link rel="shortcut icon" href="/pssp/icon.png">
     @vite('resources/sass/app.scss')
 </head>
